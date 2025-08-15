04c74ea0e327340c3b2a1cd9944de9d2108c68e7
Nawat Ngerncham <n.ngerncham@gmail.com>
2025-08-15 09:50:53 +0700

    feat: built-in vs custom functions comparison

--- outputs/03/compile_results.py
+++ outputs/03/compile_results.py
@@ -7,11 +7,9 @@ df = pd.DataFrame(pd.read_csv("run_times.csv"))
 # Concatenate the two DataFrames into a single DataFrame
 df = df.groupby("func_type")["run_time"].mean()
 
-pandas_v_polars_df = df.loc[["pandas", "polars"]]
-
 # Create a box plot to visualize the run times
 plt.figure(figsize=(5, 3))
-pandas_v_polars_df.plot(kind="bar")
+df.plot(kind="bar")
 plt.title("Time spent aggregating")
 plt.xlabel("Function Type")
 plt.ylabel("Run time (seconds)")
@@ -19,6 +17,6 @@ plt.suptitle("")
 plt.tight_layout()
 
 # Save the plot to a file
-file_name = "steam_review_builtin_v_custom.jpg"
+file_name = "steam_review_custom_v_builtin.jpg"
 plt.savefig(f"../../slides/assets/results/{file_name}")
 print(f"Box plot successfully generated and saved as {file_name}")
